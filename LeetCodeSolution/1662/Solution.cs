4cbf1cfe50c90bb596b00686f1d83a289d9e8483
Rafael Valério <rafael.valerio@outlook.com.brgit status~git config --global user.email rafael.valerio@outlook.com.br>
2023-07-07 00:54:32 -0300

    Problem 1662

---
+++ LeetCodeSolution/1662/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace _1662;
+
+public class Solution
+{
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+    {
+        string word1Result = "";
+        string word2Result = "";
+
+        for (int i = 0; i < word1.Length; i++)
+        {
+            word1Result = String.Concat(word1Result, word1[i]);
+        }
+        for (int i = 0; i < word2.Length; i++)
+        {
+            word2Result = String.Concat(word2Result, word2[i]);
+        }   
+            
+        return word1Result == word2Result;
+    }
+}
---
+++ LeetCodeSolution/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolution;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = new _1662.Solution();
+
+        string[] word1 = { "ab", "c" };
+        string[] word2 = { "a", "bc" };
+
+        var result = solution.ArrayStringsAreEqual(word1,word2);
+
+        Console.WriteLine(result);
+
+    }
+}
