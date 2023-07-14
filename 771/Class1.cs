492c32718623f3b39aa74ad2f9df583f9694da08
Rafael Valério <rafael.valerio@outlook.com.brgit status~git config --global user.email rafael.valerio@outlook.com.br>
2023-07-14 01:20:09 -0300

    771

---
+++ 771/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace _771
+{
+    public class Solution
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            int count = 0;
+            foreach (char cS in stones) 
+            {
+                foreach (char cJ in jewels)
+                {
+                    if (cS == cJ)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
